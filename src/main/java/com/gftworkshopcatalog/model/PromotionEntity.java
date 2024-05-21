fc2d07eb8894824418af30622ead73930d49eabf
Cecilia.Cortes <cecilia.cortes@gft.com>
2024-05-21 08:57:32 +0200

    Configure gitignore

--- src/main/java/com/gftworkshopcatalog/model/PromotionEntity.java
+++ src/main/java/com/gftworkshopcatalog/model/PromotionEntity.java
@@ -1,10 +1,7 @@
 package com.gftworkshopcatalog.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -13,6 +10,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 @Entity
+@Generated
 @Table(name= "promotions")
 public class PromotionEntity {
     @Id
